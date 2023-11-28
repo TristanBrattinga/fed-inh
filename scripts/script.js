952219e5fefbe70ca29148aeb1f129ae615d8fd9
TristanBrattinga <tristan.brattinga@gmail.com>
2023-11-29 00:12:45 +0100

    page wip

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,17 @@
-// JavaScript Document
-console.log("hi");
+document.addEventListener('DOMContentLoaded', function () {
+  const slider = document.querySelector('.infoSlider>div>ul');
+  let currentIndex = 0;
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % slider.children.length;
+    updateSlider();
+  }
+
+  function updateSlider() {
+    const translateValue = -currentIndex * 100 + '%';
+    slider.style.transition = 'transform 0.6s'; // Set the transition duration to 0.3 seconds (adjust as needed)
+    slider.style.transform = 'translateX(' + translateValue + ')';
+  }
+
+  setInterval(nextSlide, 6000);
+});
