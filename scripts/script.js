6b4ed409ccf584926487da3a9e68414b8528a8bc
TristanBrattinga <tristan.brattinga@gmail.com>
2023-12-05 21:07:55 +0100

    page 2 wip

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,29 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const slider = document.querySelector('.infoSlider>div>ul');
-  let currentIndex = 0;
+const slider = document.querySelector('.infoSlider>div>ul')
+let currentIndex = 0
 
-  function nextSlide() {
-    currentIndex = (currentIndex + 1) % slider.children.length;
-    updateSlider();
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slider.children.length
+  updateSlider()
+}
+
+function updateSlider() {
+  const translateValue    = -currentIndex * 100 + '%'
+  slider.style.transition = 'transform 0.6s'
+  slider.style.transform  = 'translateX(' + translateValue + ')'
+}
+
+setInterval(nextSlide, 6000)
+
+const stickyBar = document.querySelector('.productHeader')
+
+const handleScrollDirection = () => {
+  const currentPosition = window.scrollY
+
+  if (currentPosition >= 164) {
+    stickyBar.classList.add('sticky')
+  } else {
+    stickyBar.classList.remove('sticky')
   }
+}
 
-  function updateSlider() {
-    const translateValue = -currentIndex * 100 + '%';
-    slider.style.transition = 'transform 0.6s'; // Set the transition duration to 0.3 seconds (adjust as needed)
-    slider.style.transform = 'translateX(' + translateValue + ')';
-  }
-
-  setInterval(nextSlide, 6000);
-});
+window.addEventListener('scroll', handleScrollDirection)
