3d782eb73c1c6d4ee5d6bd7209398f3b386ff481
TristanBrattinga <tristan.brattinga@gmail.com>
2023-12-26 16:07:33 +0100

    sidebar wip

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,22 @@
 const slider = document.querySelector('.infoSlider>div>ul')
+const stickyBar = document.querySelector('.productHeader')
+const sidebar = document.querySelector('.productGrid>aside')
+const sidebarButton = document.querySelector('.productHeader>ul>li>button')
+
 let currentIndex = 0
 
-function nextSlide() {
+const nextSlide = () => {
   currentIndex = (currentIndex + 1) % slider.children.length
   updateSlider()
 }
 
-function updateSlider() {
+const updateSlider = () => {
   const translateValue    = -currentIndex * 100 + '%'
   slider.style.transition = 'transform 0.6s'
   slider.style.transform  = 'translateX(' + translateValue + ')'
 }
 
 setInterval(nextSlide, 6000)
-
-const stickyBar = document.querySelector('.productHeader')
 
 const handleScrollDirection = () => {
   const currentPosition = window.scrollY
@@ -27,3 +29,10 @@ const handleScrollDirection = () => {
 }
 
 window.addEventListener('scroll', handleScrollDirection)
+
+const slideOutSidebar = () => {
+  sidebar.classList.toggle('slideOut')
+}
+
+sidebarButton.addEventListener('click', slideOutSidebar)
+
