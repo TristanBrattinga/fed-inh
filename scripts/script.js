f537c20b6f254f90da7a0a5ba918767b8cd8fbc6
TristanBrattinga <tristan.brattinga@gmail.com>
2024-01-23 20:22:52 +0100

    final commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,10 @@
 const body             = document.body
-const infoSlider       = document.querySelector('.infoSlider>ul')
-const stickyBar        = document.querySelector('.productHeader')
 const sidebar          = document.querySelector('.productGrid>aside')
-const sidebarButton    = document.querySelector('.productHeader ul li button')
-const productGrid = document.querySelector('.productGrid>section')
-// const header          = document.querySelector('header>nav')
-const topBar           = document.querySelector('header>section')
+const sidebarButton    = document.querySelector('.productHeader ul li:first-of-type button')
+const productGrid      = document.querySelector('.productGrid>section')
 const productSlider    = document.querySelector('.productSlider ul:nth-child(2)')
-const product          = document.querySelector('.productSlider ul:nth-child(2) li')
 const prevButton       = document.querySelector('.productSlider li:last-child li:first-child button:first-child')
 const nextButton       = document.querySelector('.productSlider li:last-child li:last-child button:last-child')
-const shoeSlider       = document.querySelector('.shoeCategorySlider ul:nth-child(2)')
-// const shoeListItems    = shoeSlider.querySelectorAll('ul li')
 const sideBarMenu      = document.querySelector('header > nav > aside')
 const closeMenuButton  = document.querySelector('header > nav > aside > button')
 const openMenuButton   = document.querySelector('header > nav > ul > li:last-child > ul > li:last-child')
@@ -25,7 +18,7 @@ const toggleMenu = () => {
 }
 
 openMenuButton.addEventListener('click', toggleMenu)
-// closeMenuButton.addEventListener('click', toggleMenu)
+closeMenuButton.addEventListener('click', toggleMenu)
 
 body.addEventListener('click', (event) => {
   if (event.target === body) {
@@ -33,127 +26,63 @@ body.addEventListener('click', (event) => {
   }
 })
 
-const toggleModal = () => {
-  modal.classList.toggle('showModal')
-  body.classList.toggle('showMenu')
+if (modal) {
+  const toggleModal = () => {
+    modal.classList.toggle('showModal')
+    body.classList.toggle('showMenu')
+  }
+
+  closeModalButton.addEventListener('click', toggleModal)
+
+  if (localStorage.getItem('modalShown') !== 'true') {
+    setTimeout(() => {
+      toggleModal()
+      localStorage.setItem('modalShown', 'true')
+    }, 5000)
+  }
 }
 
-closeModalButton.addEventListener('click', toggleModal)
+/////////////// FILTER SIDEBAR TOGGLE ///////////////
 
-if (localStorage.getItem('modalShown') !== 'true') {
-  setTimeout(() => {
-    toggleModal()
-    localStorage.setItem('modalShown', 'true')
-  }, 5000)
+if (sidebar) {
+  const slideOutSidebar = () => {
+    sidebar.classList.toggle('slideOut')
+    productGrid.classList.toggle('slideOut')
+  }
+
+  sidebarButton.addEventListener('click', slideOutSidebar)
 }
 
-/////////////// SIDEBAR TOGGLE ///////////////
+let currentIndex = 0
+const maxIndex   = productSlider.children.length - 1
 
-const slideOutSidebar = () => {
-  sidebar.classList.toggle('slideOut')
-  productGrid.classList.toggle('slideOut')
+const updateSlider = () => {
+  productSlider.scrollLeft = currentIndex * 500
 }
 
-sidebarButton.addEventListener('click', slideOutSidebar)
+const updateButtons = () => {
+  if (currentIndex === 0) {
+    prevButton.classList.add('disabled')
+    nextButton.classList.remove('disabled')
+  } else if (currentIndex === maxIndex) {
+    prevButton.classList.remove('disabled')
+    nextButton.classList.add('disabled')
+  } else {
+    prevButton.classList.remove('disabled')
+    nextButton.classList.remove('disabled')
+  }
+}
 
+updateButtons()
 
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + (maxIndex + 1)) % (maxIndex + 1)
+  updateSlider()
+  updateButtons()
+})
 
-
-// shoeListItems.forEach(function(item, index) {
-//   const span       = item.querySelector('figure span')
-//   span.textContent = (index + 1) + '/' + shoeListItems.length
-//
-//   item.style.border = '2px solid red'
-// })
-
-// const prevShoeButton = document.querySelector('.shoeCategorySlider button:first-child');
-// const nextShoeButton = document.querySelector('.shoeCategorySlider button:last-child');
-//
-// const itemWidth = shoeSlider.firstElementChild.offsetWidth; // Assumes all items have the same width
-//
-// prevShoeButton.addEventListener('click', () => {
-//   shoeSlider.scrollBy({
-//     left: itemWidth,
-//     behavior: 'smooth'
-//   });
-// });
-//
-// nextShoeButton.addEventListener('click', () => {
-//   shoeSlider.scrollBy({
-//     left: itemWidth,
-//     behavior: 'smooth'
-//   });
-// });
-
-// let currentIndex = 0
-// const maxIndex   = productSlider.children.length - 1
-
-// const updateSlider = () => {
-//   productSlider.scrollLeft = currentIndex * 500
-// }
-//
-// const updateButtons = () => {
-//   if (currentIndex === 0) {
-//     prevButton.classList.add('disabled')
-//     nextButton.classList.remove('disabled')
-//   } else if (currentIndex === maxIndex) {
-//     prevButton.classList.remove('disabled')
-//     nextButton.classList.add('disabled')
-//   } else {
-//     prevButton.classList.remove('disabled')
-//     nextButton.classList.remove('disabled')
-//   }
-// }
-//
-// updateButtons()
-//
-// prevButton.addEventListener('click', () => {
-//   currentIndex = (currentIndex - 1 + (maxIndex + 1)) % (maxIndex + 1)
-//   updateSlider()
-//   updateButtons()
-// })
-//
-// nextButton.addEventListener('click', () => {
-//   currentIndex = (currentIndex + 1) % (maxIndex + 1)
-//   updateSlider()
-//   updateButtons()
-// })
-
-// const handleScrollDirection = () => {
-//   const currentPosition = window.scrollY
-//
-//   if (currentPosition >= 164) {
-//     stickyBar.classList.add('sticky')
-//   } else {
-//     stickyBar.classList.remove('sticky')
-//   }
-// }
-//
-// let previousScrollPosition = 0
-//
-// const handleScroll = () => {
-//   const rect = header.getBoundingClientRect()
-//   const headerYTop = rect.top
-//   const currentPosition = window.scrollY
-//   const topBarRect = topBar.getBoundingClientRect()
-//   const topBarYBottom = topBarRect.bottom
-//
-//   if (currentPosition - 38 > headerYTop) {
-//     header.classList.add('slide-out')
-//     header.classList.remove('fixed')
-//   } else {
-//     header.classList.remove('slide-out')
-//   }
-//
-//   if (currentPosition < previousScrollPosition) {
-//     header.classList.add('fixed')
-//   } else {
-//     header.classList.remove('fixed')
-//   }
-//
-//   previousScrollPosition = currentPosition
-//
-// }
-//
-//
-// window.addEventListener('scroll', handleScroll)
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % (maxIndex + 1)
+  updateSlider()
+  updateButtons()
+})
